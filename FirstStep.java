056999524b8fef40be02dc5bb218553c03226d3f
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-10 01:36:25 +0300

    Add files via upload

---
+++ FirstStep.java
@@ -0,0 +1,22 @@
+/*************************************************
+ * This is the Text-printing program.
+ * 
+ *  The program shows how to use print command.
+ *
+ * @author Burak Karacan
+ *
+ *************************************************/
+public class FirstStep { // start class FirstStep
+
+	public static void main(String[] args) { // main method begins execution Java apps.
+		
+		System.out.println("The vast majority of people do not want to swim without learning how to swim.\n " // print to display...
+				+ "What a meaningful word, right?\n "
+				+ "It is natural that they do not want to swim, because they were created to live on land, not in water.\n "
+				+ "And it is natural that they do not want to think, because they were created to live, not to think!\n "
+				+ "Yes, whoever thinks, who makes thinking his main occupation, can reach an advanced point in this;\n "
+				+ "However, such a person has exchanged land and water, and one day he will drown in the water.\n");
+		
+	} // end of the main method
+
+} // end of the FirstStep class
