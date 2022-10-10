6b6a9b3b77c0ab28d9d4223b042b0edf4d39ba50
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-10 17:23:55 +0300

    Add files via upload

---
+++ SecondStep.java
@@ -0,0 +1,19 @@
+/*************************************************
+ * This is a Text-printing program. 
+ * 
+ *  The program shows how to print a line with multiple statements.
+ *
+ * @author Burak Karacan
+ *
+ *************************************************/
+public class SecondStep {
+
+	public static void main(String[] args) { // Main method begins to execute of Java app.
+		
+		System.out.print("The weather is ");
+		System.out.println("partly cloudy today.");
+		
+
+	} //End of main method...
+
+} // End of class...
