580a3308ee6c546b437d2d9aa512d674bd768b3c
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-12 02:19:12 +0300

    Add files via upload

---
+++ Exercise2_3.java
@@ -0,0 +1,54 @@
+/*************************************************************************************
+ * This program includes the answers that exist on page 115 
+ * 
+ *  Exercise 2.3 
+ * 
+ * @author Burak Karacan
+ *
+ ************************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercise2_3 {
+
+	public static void main(String[] args) {
+		
+		try (Scanner input = new Scanner(System.in)) {
+			
+			
+			int c = 0;						// Valid variable.
+			
+			int thisIsAVariable = 0;		// Valid variable.
+			
+			int q76354 = 0;					// valid variable.
+			
+			
+			System.out.print("ENTER AN INTEGER : ");
+			
+			int userValue = input.nextInt();
+			
+			System.out.println("This is a JAVA program.");
+			
+			String str1 = "This is a JAVA";
+			
+			String str2 = "program.";
+			
+			System.out.printf("%s%n%s%n", str1, str2); 		// %n ----> goes to the bottom line.
+			
+
+			
+			if(userValue != 7)
+				
+				
+			{														
+				
+				System.out.println("THE USER NUMBER IS NOT EQUAL TO 7!");
+																				
+																				}
+		}			
+		
+
+	}
+
+}
+
