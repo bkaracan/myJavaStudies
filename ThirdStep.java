8b5df3b4ea0d1e262a0c37ca50b56972f8c168cb
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-11 19:56:54 +0300

    Add files via upload

---
+++ ThirdStep.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * This is a Text-printing program with \n
+ * 
+ * \n ---> allows you to go to the bottom line in the same println command
+ * 
+ *  The program shows how to print multiple lines with a single statement.
+ *
+ * @author Burak Karacan
+ *
+ ******************************************************************************/
+public class ThirdStep {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is the first line.\nThis is the second line.\nThis is the third line.");
+
+	}
+
+}
