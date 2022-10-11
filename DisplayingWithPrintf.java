49bd49d7a2d35905cd0c208c2c6f30787e563b38
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-11 20:18:59 +0300

    Add files via upload

---
+++ DisplayingWithPrintf.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * This is a Text-printing program with printf command.
+ * 
+ * %s ---> allows you to print string expressions.
+ * 
+ *  In this code printf command has 3 string expressions.
+ *
+ * @author Burak Karacan
+ *
+ ******************************************************************************/
+public class DisplayingWithPrintf {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("%s%s%s", "TOYOTA", " ",  "SUUUPPPRAAAAA!");
+
+	}
+
+}
