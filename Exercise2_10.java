6c4d1400734eaf313090b219d0ee62fe04a7c0b1
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-12 11:21:52 +0300

    Update Exercise2_10.java

--- Exercise2_10.java
+++ Exercise2_10.java
@@ -2,6 +2,8 @@
  * Lets get some practice in printf format.
  *  
  *  Exercise 2.10 / it's on page 115.
+ *  
+ *  The program includes Exercise 2.13!
  * 
  * @author Burak Karacan
  *
@@ -14,11 +16,17 @@ public class Exercise2_10 {
 		
 		int y = 1;
 		
-		System.out.printf("x = %d\n", x + 5); 		// prints "x = 10"
+		int p = 5;
 		
-		 System.out.printf("Value of %d * %d is %d\n", x, y, (x * y));			// prints "Value of 5 * 1 is 5"
+		System.out.printf("x = %d\n\n", x + 5); 		// prints "x = 10"
+		
+		 System.out.printf("Value of %d * %d is %d\n\n", x, y, (x * y));			// prints "Value of 5 * 1 is 5"
 		 
-		 System.out.printf("%d is not equal to %d\n", (x + y), (x * y));		// prints "6 is not equal to 5"
+		 System.out.printf("%d is not equal to %d\n\n", (x + y), (x * y));		// prints "6 is not equal to 5"
+		 
+		 System.out.printf("%d\n\n", p + 2 * 4);			// prints "13"
+		 
+		 System.out.printf("%d", p * 2 + 4);			// prints "14"
 		
 	}
 
