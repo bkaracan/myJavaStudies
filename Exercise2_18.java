54f39cdc13e045387525b8880e445c7abf0632af
bkaracan <79939265+bkaracan@users.noreply.github.com>
2022-10-12 19:47:10 +0300

    Add files via upload

---
+++ Exercise2_18.java
@@ -0,0 +1,18 @@
+
+public class Exercise2_18 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("*********\t***\t\s\s\s*\t\s\s\s\s\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s***\t\s\s\s\s\s\s\s*\s\s\s*");
+		System.out.println("*\t*\s\s\s\s*\s\s\s\s\s\s\s*\s\s\s******\s\s\s\s\s\s\s*\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s*\s\s\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s\s\s\s\s*\s\s\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s*\s\s\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s\s\s\s*\s\s\s\s\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s*\s\s\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s\s\s\s\s*\s\s\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s*\s\s\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s\s\s\s\s\s*\s\s\s\s\s*");
+		System.out.println("*\t*\s\s\s\s\s*\s\s\s\s\s*\s\s\s\s\s\s*\s\s\s\s\s\s\s\s\s\s\s*\s\s\s*");
+		System.out.println("*********\t***\t\s\s\s*\t\s\s\s\s\s\s\s\s\s*");
+
+	}
+
+}
