cb96927acfbef550edf6732c830b4b35b304d289
Burak Karacan <79939265+bkaracan@users.noreply.github.com>
2022-10-19 16:54:17 +0300

    Add files via upload

---
+++ Exercise3_3.java
@@ -0,0 +1,28 @@
+/*************************************************************************************
+ * Exercise 3_3 is on page 155.
+ * 
+ *Write four different Java statements that each add 1 to integer variable x
+ * 
+ * @author Burak Karacan
+ *
+ ************************************************************************************/
+
+public class Exercise3_3 {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		x = x + 1; // x = 6
+		
+		x += 1; // x = 7
+		
+		x ++; // x = 8
+		
+		++ x; // x = 9
+		
+		System.out.println(x);
+
+	}
+
+}
