c3dceacb4aca44ed97a1bbea912156cab82bca11
Burak Karacan <79939265+bkaracan@users.noreply.github.com>
2022-10-19 17:14:59 +0300

    Add files via upload

---
+++ Exercise3_5.java
@@ -0,0 +1,32 @@
+/*********************************************************************************************
+ * Exercise 3_5 is on page 155.
+ 
+ *Write a Java statement to accomplish each of the following tasks:
+
+ * Declare variable sum of type int and initialize it to 0.
+ 
+ *Declare variable x of type int and initialize it to 1.
+
+ * Add variable x to variable sum, and assign the result to variable sum.
+
+ *Print "The sum is: ", followed by the value of variable sum.
+ 
+ * @author Burak Karacan
+ *
+ ***********************************************************************************************/
+
+public class Exercise3_5 {
+
+	public static void main(String[] args) {
+		 
+		int sum = 0;
+		
+		int x = 1;
+		
+		sum += x;
+		
+		System.out.println(sum);
+
+	}
+
+}
